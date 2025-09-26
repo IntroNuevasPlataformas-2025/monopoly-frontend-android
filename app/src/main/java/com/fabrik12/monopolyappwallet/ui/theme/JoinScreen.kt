ac9e0b01b962f26ed8427dbff97eb1ddd6c13b38
fabrik12 <fbalarezo@unsa.edu.pe>
2025-09-26 14:55:18 -0500

    Agregada la pantalla JoinScreen
    Se agregó el boceto de la pantalla `JoinScreen` en el archivo `app/src/main/java/com/fabrik12/monopolyappwallet/ui/theme/JoinScreen.kt` para permitir a los usuarios unirse a un juego.

---
+++ app/src/main/java/com/fabrik12/monopolyappwallet/ui/theme/JoinScreen.kt
@@ -0,0 +1,15 @@
+package com.fabrik12.monopolyappwallet.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun JoinScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JoinScreenPreview() {
+    JoinScreen();
+}
