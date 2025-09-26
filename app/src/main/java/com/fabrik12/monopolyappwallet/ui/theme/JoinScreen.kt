b9c3e5c935ebb243435dcc2bb4c858290bc4f68b
fabrik12 <fbalarezo@unsa.edu.pe>
2025-09-26 15:30:00 -0500

    Actualizada la pantalla JoinScreen
    Se agregaron campos de texto para el nombre del jugador y el ID de la partida, junto con un botón para unirse.

--- app/src/main/java/com/fabrik12/monopolyappwallet/ui/theme/JoinScreen.kt
+++ app/src/main/java/com/fabrik12/monopolyappwallet/ui/theme/JoinScreen.kt
@@ -1,11 +1,58 @@
 package com.fabrik12.monopolyappwallet.ui
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun JoinScreen() {
+    // Memoria para campo de texto
+    val playerName = remember { mutableStateOf("") }
+    val gameId = remember { mutableStateOf("") }
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Campo de texto para el nombre del Jugador
+        OutlinedTextField(
+            value = playerName.value,
+            onValueChange = { playerName.value = it },
+            label = { Text("Tu Nombre") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Campo de texto para el ID de partida
+        OutlinedTextField(
+            value = gameId.value,
+            onValueChange = { gameId.value = it },
+            label = { Text("ID de la Partida") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                /* Logica con Websocket */
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Unirse a la Partida")
+        }
+    }
 }
 
 @Preview(showBackground = true)
