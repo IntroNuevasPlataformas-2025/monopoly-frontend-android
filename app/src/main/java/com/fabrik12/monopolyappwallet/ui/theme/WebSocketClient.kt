d5cd924ed40bd815e9d2bb9ab1b1a4bdecb508d8
fabrik12 <fbalarezo@unsa.edu.pe>
2025-09-26 18:53:32 -0500

    Agregado el objeto WebSocketClient
    Se creó el objeto `WebSocketClient` en el archivo `app/src/main/java/com/fabrik12/monopolyappwallet/ui/theme/WebSocketClient.kt` para gestionar la conexión WebSocket. Este objeto incluye métodos para conectar, enviar mensajes y desconectar del servidor.

---
+++ app/src/main/java/com/fabrik12/monopolyappwallet/ui/theme/WebSocketClient.kt
@@ -0,0 +1,64 @@
+package com.fabrik12.monopolyappwallet.ui.theme
+
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+
+object WebSocketClient {
+    private const val SERVER_URL = "ws://localhost:3000"
+
+    private lateinit var client: OkHttpClient
+    private var webSocket: WebSocket? = null
+
+    fun connect() {
+        client = OkHttpClient()
+        val request = Request.Builder().url(SERVER_URL).build()
+
+        // Crear Listener
+        val listener = object : WebSocketListener() {
+            override fun onOpen(webSocket: WebSocket, response: Response) {
+                Log.d("WebSocket", "Conexión abierta")
+            }
+
+            override fun onMessage(webSocket: WebSocket, text: String) {
+                Log.d("WebSocket", "Mensaje recibido: $text")
+                // Manejar el mensaje recibido
+            }
+
+            override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+                webSocket.close(1000, null)
+                Log.d("WebSocket", "Cerrando conexión: $code / $reason")
+            }
+
+            override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                Log.e("WebSocket", "Error en la conexion: ${t.message}")
+            }
+        }
+
+        // Iniciar la conexion
+        webSocket = client.newWebSocket(request, listener)
+    }
+
+    fun sendMessage(playerName: String, gameId: String) {
+        // Construir el mensaje JSON
+        val jsonPayload = JSONObject().apply {
+            put("playerId", "android-${System.currentTimeMillis()}")
+            put("playerName", playerName)
+            put("gameId", gameId)
+        }
+
+        val jsonMessage = JSONObject().apply {
+            put("type", "JOIN_GAME")
+            put("payload", jsonPayload)
+        }
+
+        val message = jsonMessage.toString()
+        webSocket?.send(message)
+        Log.d("WebSocket", "Mensaje enviado: $message")
+
+    }
+
+    fun disconnect() {
+        webSocket?.close(1000, "Cierre normal")
+    }
+}
