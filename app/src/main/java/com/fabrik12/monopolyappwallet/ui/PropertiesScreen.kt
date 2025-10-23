f3fbb7039e8194470f093e28160f0025a49c0397
fabrik12 <fbalarezo@unsa.edu.pe>
2025-10-23 10:34:45 -0500

    Refactorizada la pantalla de propiedades con `LazyColumn`
    Se ha actualizado la `PropertiesScreen` para mostrar una lista de propiedades utilizando `LazyColumn`, lo que mejora la eficiencia en el renderizado de listas largas. Cada elemento de la lista se representa a través del composable `PropertyItem`.
    
    Cambios principales:
    - Se reemplazó el texto de marcador de posición por un `LazyColumn`.
    - Se utiliza `mockPropertyList` como fuente de datos para poblar la lista.
    - Se agregó un `Spacer` al final de la lista para mejorar el espaciado visual.
    - Se añadió un marcador de posición para la barra de navegación inferior (`bottomBar`).

--- app/src/main/java/com/fabrik12/monopolyappwallet/ui/PropertiesScreen.kt
+++ app/src/main/java/com/fabrik12/monopolyappwallet/ui/PropertiesScreen.kt
@@ -1,11 +1,19 @@
 package com.fabrik12.monopolyappwallet.ui
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fabrik12.monopolyappwallet.data.mockPropertyList
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -13,10 +21,22 @@ fun PropertiesScreen() {
     Scaffold(
         topBar = {
             TopAppBar(title = { Text("Propiedades del Tablero") })
+        },
+        bottomBar = {
+            // Barra de Navegacion (inferior)
+            Text("Barra de Navegacion Inferior (proto)")
         }
     ) { innerPadding ->
         // Contenido de la pantalla
-        Text("Contenido de la pantalla de Propiedades")
+
+        LazyColumn(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            items(mockPropertyList) { property ->
+                PropertyItem(property = property)
+            }
+        }
+        Spacer(modifier = Modifier.height(24.dp)) // Espacio final
 
     }
 }
