918378ab57387ed4b7c2bd9988997b4a077faeb8
fabrik12 <fbalarezo@unsa.edu.pe>
2025-10-23 11:55:05 -0500

    Dependencia para navegacion actualizada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation(libs.okhttp)
     implementation("androidx.compose.material:material-icons-extended:1.6.0")
+    implementation("androidx.compose.material3:material3:1.4.0")
 }
