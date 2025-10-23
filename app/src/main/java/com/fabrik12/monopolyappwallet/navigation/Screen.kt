4c639debcb051c569d8b98f30b92cc1f51b0350d
fabrik12 <fbalarezo@unsa.edu.pe>
2025-10-23 10:41:01 -0500

    Definidas las pantallas de navegación de la aplicación
    Se ha creado el archivo `Screen.kt` para centralizar las definiciones de las distintas pantallas de la aplicación y sus propiedades de navegación.
    
    Se ha definido una clase sellada `Screen` que incluye la ruta (`route`), la etiqueta (`label`) y el icono (`icon`) para cada pantalla. Las pantallas iniciales definidas son:
    
    -   `Game`: Pantalla principal del juego.
    -   `Actions`: Pantalla para las acciones del jugador.
    -   `Properties`: Pantalla para la gestión de propiedades.

---
+++ app/src/main/java/com/fabrik12/monopolyappwallet/navigation/Screen.kt
@@ -0,0 +1,29 @@
+package com.fabrik12.monopolyappwallet.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.PlayCircle
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class Screen(
+    val route: String,
+    val label: String,
+    val icon: ImageVector
+) {
+    object Game: Screen(
+        route = "game_screen",
+        label = "Juego",
+        icon = Icons.Default.Home
+    )
+    object Actions: Screen(
+        route = "actions_screen",
+        label = "Acciones",
+        icon = Icons.Default.PlayCircle
+    )
+    object Properties: Screen(
+        route = "properties_screen",
+        label = "Propiedades",
+        icon = Icons.Default.List
+    )
+}
